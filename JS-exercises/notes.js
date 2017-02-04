ecd054df2cb8be2835a31b51328ad4e4d941f85f
GoForMarty <marty.stepien@gmail.com>
2017-02-04 23:35:05 +0100

    Second for loop added

--- JS-exercises/notes.js
+++ JS-exercises/notes.js
@@ -65,3 +65,18 @@ for (var i = vacationSpots.length -1; i >= 0; i--) {
   console.log('I would love to visit ' + vacationSpots[i]);
 }
 
+
+var myPlaces = ['Paris', 'New York', 'Barcelona'];
+var friendPlaces = ['Rome', 'Chicago', 'Paris'];
+
+for (var i = 0; i < myPlaces.length; i++) {
+    console.log(myPlaces[i]);
+  
+  for (var j = 0; j < friendPlaces.length; j++) {
+        console.log(friendPlaces[j]);
+    
+    if (myPlaces[i] === friendPlaces[j]) {
+    			console.log('Match: ' + myPlaces[i]);
+				}
+    }
+}
