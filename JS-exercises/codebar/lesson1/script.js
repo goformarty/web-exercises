f3011943084ba037ca6b9f38f819fd44393ee2d1
GoForMarty <marty.stepien@gmail.com>
2017-02-21 21:54:56 +0100

    Greeting functions added

--- JS-exercises/codebar/lesson1/script.js
+++ JS-exercises/codebar/lesson1/script.js
@@ -1,5 +1,15 @@
 // This is where all the practise code from the Introduction to Javascript tutorial goes
 
-function hello() {
-	console.log("Hello!");
+function hello(person) {
+	console.log("Hello " + person + "!");
+}
+
+function conversation(person) {
+	hello(person);
+	console.log('How are you?');
+	console.log('Goodbye!');
+}
+
+function greeting(person) {
+	return 'Hello ' + person + '!';
 }
