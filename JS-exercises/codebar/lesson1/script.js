5066ef04880cbdc5fb5d472bf25071d5faf2262d
GoForMarty <marty.stepien@gmail.com>
2017-02-21 22:08:34 +0100

    References

--- JS-exercises/codebar/lesson1/script.js
+++ JS-exercises/codebar/lesson1/script.js
@@ -13,3 +13,20 @@ function conversation(person, topic) {
 function greeting(person) {
 	return 'Hello ' + person + '!';
 }
+
+var me_a = {
+	first_name: 'Marty',
+	likes: 'tigers'
+};
+
+var me_b = me_a;
+
+console.log('Before');
+console.log(me_a.first_name);
+console.log(me_b.first_name);
+
+me_a.first_name = 'Martyna';
+
+console.log('After');
+console.log(me_a.first_name);
+console.log(me_b.first_name);
