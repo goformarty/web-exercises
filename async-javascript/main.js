4462a2560e401631b29cb41b55d537491335adc4
Marty <marty.stepien@gmail.com>
2017-09-21 18:13:11 +0100

    basic promise and then chained

--- async-javascript/main.js
+++ async-javascript/main.js
@@ -1,7 +1,30 @@
-function resolveAfter2Seconds(x) {
-    return new Promise( resolve => {
-        setTimeout(() => {
-            resolve(x);
-        }, 2000);
-    });
-}
+let myFirstPromise = new Promise((resolve, reject) => {
+    setTimeout(function(){
+      resolve("Success!");
+    }, 250);
+  });
+  
+  myFirstPromise.then((successMessage) => {
+    console.log("Yay! " + successMessage);
+  });
+
+// function resolveAfter2Seconds(x) {
+//     return new Promise( resolve => {
+//         setTimeout(() => {
+//             resolve(x);
+//         }, 2000);
+//     });
+// }
+
+// async function f1() {
+//     const x = await resolveAfter2Seconds(10);
+//     console.log(x);
+// }
+
+// f1();
+
+// async function add1(x) {
+//     const a = await resolveAfter2Seconds(20);
+//     const b = await resolveAfter2Seconds(30);
+//     return x + a + b;
+// }
