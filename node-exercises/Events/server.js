7bd1f83fef730f4743256ed1c7fd81d7b72dc9ae
Marty Stepien <marty.stepien@gmail.com>
2017-11-14 16:02:10 +0000

    express added to airline

--- node-exercises/Events/server.js
+++ node-exercises/Events/server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var handleRequest = function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end("Node server's running!\n");
+	res.end("Node server's running! \n");
 };
 
 var server = http.createServer(handleRequest);
